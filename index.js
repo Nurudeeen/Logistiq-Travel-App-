49c77942727a0f7ebf50de2d5ecc509330c4c3a3
Nurudeeen <nurudeenrabiu81@gmail.com>
2022-02-27 13:02:44 +0100

    app working as one-one origin and destination

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 const port = process.env.PORT || 3000
 require ("dotenv").config();
 
@@ -12,5 +20,5 @@ app.use("/magic", require("./routes/LogistiqRoute"))
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Magic at port ${port}`)
 })
--- routes/LogistiqRoute.js
+++ routes/LogistiqRoute.js
@@ -2,11 +2,31 @@ const express = require('express')
 const router = express.Router()
 var distance = require('google-distance-matrix');
 distance.key(process.env.API_KEY);
-distance.units('metric');
-var origins = ['San Francisco CA', '40.7421,-73.9914'];
-var destinations = ['New York NY', 'Montreal', '41.8337329,-87.7321554', 'Honolulu'];
 
-router.get('/', (req, res) => {
+distance.units('metric');
+// distance.mode('transit');
+// distance.transit_mode('bus');
+// var dat = "Boston"
+// var origins = ['San Francisco CA'];
+// origins.push (dat)
+// //console.log(origins)
+// var destinations = ['New York NY'];
+
+router.post('/', (req, res) => {
+
+    const origins = []
+    origins[0] = req.body.origin
+
+    const destinations = []
+    destinations[0] = req.body.destination
+
+    if (req.body.transit === true) {
+     distance.mode('transit');
+     distance.transit_mode('bus');
+    }else{
+        distance.mode('driving')
+        //return
+    }
     
     distance.matrix(origins, destinations, function (err, distances) {
         if (err) {
@@ -16,13 +36,16 @@ router.get('/', (req, res) => {
             return console.log('no distances');
         }
         if (distances.status == 'OK') {
+            res.json(distances);
             for (var i=0; i < origins.length; i++) {
                 for (var j = 0; j < destinations.length; j++) {
                     var origin = distances.origin_addresses[i];
                     var destination = distances.destination_addresses[j];
                     if (distances.rows[0].elements[j].status == 'OK') {
                         var distance = distances.rows[i].elements[j].distance.text;
+                        var time = distances.rows[i].elements[j].duration.text;
                         console.log('Distance from ' + origin + ' to ' + destination + ' is ' + distance);
+                        console.log('Time of travel from ' + origin + ' to ' + destination + ' is ' + time);
                     } else {
                         console.log(destination + ' is not reachable by land from ' + origin);
                     }

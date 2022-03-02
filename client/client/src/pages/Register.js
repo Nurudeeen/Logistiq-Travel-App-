d412e7b388e7a3ae0ac1022459c65d6540ae998e
Nurudeeen <nurudeenrabiu81@gmail.com>
2022-03-02 15:24:19 +0100

    adding react frontend

---
+++ client/client/src/pages/Register.js
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
+function App() {
+	const history = useHistory()
+
+	const [Username, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	async function registerUser(event) {
+		event.preventDefault()
+
+		const response = await fetch('http://localhost:3000/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				Username,
+				email,
+				password,
+			}),
+		})
+
+		const data = await response.json()
+
+		if (data.status === 'ok') {
+			history.push('/login')
+		}
+	}
+
+	return (
+		<div>
+			<h1>Register</h1>
+			<form onSubmit={registerUser}>
+				<input
+					value={Username}
+					onChange={(e) => setName(e.target.value)}
+					type="text"
+					placeholder="Name"
+				/>
+				<br />
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type="email"
+					placeholder="Email"
+				/>
+				<br />
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type="password"
+					placeholder="Password"
+				/>
+				<br />
+				<input type="submit" value="Register" />
+			</form>
+		</div>
+	)
+}
+
+export default App
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const db = require("./models/db")
-
+const cors = require("cors");
+app.use(cors());
 
 db.connect();
 // parse application/x-www-form-urlencoded
